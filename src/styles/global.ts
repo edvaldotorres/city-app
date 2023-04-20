79a389d549b37743b62d7be0e22f883b5e8aa3fd
Edvaldo Torres <edvaldodev@gmail.com>
2023-04-20 16:18:43 -0300

    feat: add image to the body

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,16 +1,25 @@
 import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyle = createGlobalStyle`
+  :root {
+    --pinkCities: #e02041;
+  }
+
    * {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
+    font-family: 'Anuphan', sans-serif;
   }
 
   body {
     height: 100vh;
     -webkit-font-smoothing: antialiased;
     overflow-x:hidden;
+    background: rgba(0, 0, 0, 0.5) url("https://images.unsplash.com/photo-1582741099331-e0918de0565f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80");
+    background-blend-mode: darken;
+    background-size: cover;
+    background-position: center;
   }
 
   html {
