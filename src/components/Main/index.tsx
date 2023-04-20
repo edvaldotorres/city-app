b0d72a2c430f8d76ffa6091e36471d1f0ef631f1
Edvaldo Torres <edvaldodev@gmail.com>
2023-04-20 16:18:27 -0300

    feat: add Main Component

---
+++ src/components/Main/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import { Container } from "./styles";
+
+const Main: React.FC = () => {
+  return (
+    <Container>
+      <h1>
+        Explorer os estados do Brasil e suas <span>Cidades</span>
+      </h1>
+    </Container>
+  );
+};
+
+export default Main;
---
+++ src/components/Main/styles.tsx
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  max-width: 600px;
+  width: 100%;
+
+  h1 {
+    font-size: 3rem;
+    font-weight: 600;
+    color: #fff;
+    text-shadow: 2px 2px 4px #000000;
+
+    span {
+      color: var(--pinkCities)
+    }
+
+  }
+
+  @media (max-width: 1440px) {
+    h1 {
+      text-align: center  
+    }
+`;
